3b75a42996e2d160bbc179e68e27b95545e9d5f0
Devian Kapp <forza9882@gmail.com>
2020-12-19 11:13:11 +0200

    Adding query for categories section on navigation.

--- includes/navigation.php
+++ includes/navigation.php
@@ -13,6 +13,11 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
+                <?php
+                    $query = "SELECT * FROM categories ";
+                    $select_all_categories_query = mysqli_query($connection, $query);
+                ?>
+
                     <li>
                         <a href="#">About</a>
                     </li>
